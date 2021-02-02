456ae3beda5f90d424e4e90e7dc3fac4a8b58b02
labpqsdr <huangzitong630@outlook.com>
2021-02-02 21:41:08 +0800

    Sort

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Algorithms
 
 The algorithms  written in python for Algorithms, by Robert Sedgewick, 4th edition.
+
+## Sort
+- [x] Bubble sort
+- [x] Choose sort
+- [x] Insert sort
+- [ ] Shell sort
+- [ ] Merge sort
+- [ ] Quick sort
--- sort.py
+++ sort.py
@@ -1,1 +1,31 @@
-def 
+# double cycle
+def bubbleSort(array):
+    for i in range(len(array)):
+        for j in (i+1, len(array)):
+            if array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
+    return array
+
+# find the smallest number of this array, exchange it with the first element
+def chooseSort(array):
+    for i in range(len(array)):
+        minimum = ip
+        for j in range(i+1, len(array)):
+            if array[j] < array[minimum]:
+                minimum = j
+        array[i], array[minimum] = array[minimum], array[i]
+    return array
+
+# always make the front part ordered, like playing cards
+# example: i=1, array[1] only has one element, which is ordered
+# the thing we need to do is put another element in this ordered array, and check the element we put has the right order
+# cycle this process, we can have an ordered array
+def insertSort(array):
+    for i in range(1, len(array)):
+        for j in range(i, 0, -1):
+            if array[j] < array[j-1]:
+                array[j-1], array[j] = array[j], array[j-1]
+    return array
+
+# make the elements with any interval in the dispersion are ordered
+def shellSort(array):
