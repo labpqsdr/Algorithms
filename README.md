f442765feef192bc6fe49c34b4166628f0c6f0b4
labpqsdr <huangzitong630@outlook.com>
2021-02-04 00:12:41 +0800

    merge sort

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ The algorithms  written in python for Algorithms, by Robert Sedgewick, 4th editi
 - [x] Bubble sort
 - [x] Choose sort
 - [x] Insert sort
-- [ ] Shell sort
+- [x] Shell sort
 - [ ] Merge sort
 - [ ] Quick sort
--- sort.py
+++ sort.py
@@ -41,6 +41,38 @@ def shellSort(array):
 
 # time: propotional to NlogN
 # space(extra): proptional to N
-class  mergeSort(self, array):
-    def merge()
-    
+
+
+def merge(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        L = array[:mid]
+        M = array[mid:]
+        merge(L)
+        merge(M)
+        i = j = k = 0
+
+        # Until we reach either end of either L or M, pick larger among
+        # elements L and M and place them in the correct position at A[p..r]
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
+
+        # When we run out of elements in either L or M,
+        # pick up the remaining elements and put in A[p..r]
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(M):
+            array[k] = M[j]
+            j += 1
+            k += 1
+    return array
+
