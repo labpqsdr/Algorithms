4c806a6c50119f235f68fea6e63cc08fcc8abe34
labpqsdr <huangzitong630@outlook.com>
2021-03-11 20:43:14 +0800

    quick sort

--- sort.py
+++ sort.py
@@ -40,7 +40,7 @@ def shellSort(array):
             for j in range(i, 0, -gap):
                 if array[j] < array[j-gap]:
                     array[j-gap], array[j] = array[j], array[j-gap]
-        gap = gap // 2 
+        gap = gap // 2
     return array
 
 # Merge Sort
@@ -53,7 +53,6 @@ def mergeSort(array):
         right = array[mid:]
         mergeSort(left)
         mergeSort(right)
-        
         i = j = k = 0
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
@@ -73,10 +72,32 @@ def mergeSort(array):
             k += 1
     return array
 
-if __name__ == '__main__':
-    array = [1, 24, -7, 0, 94, 234, -77, 173]
-    print(mergeSort(array))
-
 # Quick Sort
-# 
-def quickSort()
+# the most useful sort algorithms
+def quickSort(array, low, high):
+    if len(array) == 1:
+        return array
+    if low < high:
+        pi = partition(array, low, high)
+        quickSort(array, low, pi-1)
+        quickSort(array, pi+1, high)
+    return array
+
+def partition(array, low, high):
+    i = low-1
+    v = array[high]
+
+    for j in range(low, high):
+        if array[j] <= v:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i+1], array[high] = array[high], array[i+1]
+    return i+1
+
+# Quick sort(alt)
+# 1. use insertion sort when short array
+def quickSort_alt_1(array, low, high):
+    if (high <= low+5):
+        return insertionSort(array)
+    else:
+        return quickSort(array, low, high)
